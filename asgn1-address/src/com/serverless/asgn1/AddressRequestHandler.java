44797a006d75642128f9e4b3bfe40597f91f50f4
niuli0215 <ln2334@columbia.edu>
2016-09-29 15:35:41 -0400

    Add basic POST to AddressHandler
    Add basic and POST to AddressHandler

--- asgn1-address/src/com/serverless/asgn1/AddressRequestHandler.java
+++ asgn1-address/src/com/serverless/asgn1/AddressRequestHandler.java
@@ -1,16 +1,93 @@
 package com.serverless.asgn1;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.PutItemSpec;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ConditionalCheckFailedException;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class AddressRequestHandler implements RequestHandler<AddressRequest, AddressResponse> {
-
+    
+    
     @Override
-    public AddressResponse handleRequest(AddressRequest input, Context context) {
-        context.getLogger().log("Input: " + input);
-
-        // TODO: implement your handler
-        return null;
+    public AddressResponse handleRequest(AddressRequest request, Context context) {
+        AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+        DynamoDB dynamoDB = new DynamoDB(client);
+        Table Address_table = dynamoDB.getTable("Address");
+        
+        // Create operation
+        if (request.operation.equals("create")) {
+        
+            // Validate id field not null
+            if (request.item.id == null) return messageResponse("Must have id!");
+            
+            
+            // Check existence and write item to the table 
+            PutItemSpec putItemSpec = new PutItemSpec()
+                    .withItem(addAddress(request))
+                    .withConditionExpression("attribute_not_exists(id)");
+            try {
+                Address_table.putItem(putItemSpec);
+                return messageResponse("Success!");
+            } catch(ConditionalCheckFailedException e){
+                return messageResponse("Id already exists!");
+            }
+        }
+        
+        // Query operation
+        else if (request.operation.equals("query")) {
+             
+        } 
+        
+        // Update operation
+        else if (request.operation.equals("update")) {
+            
+        } 
+        
+        // Delete operation
+        else if (request.operation.equals("delete")) {
+            
+        }
+        
+        return messageResponse("Invalid Request!");
+    }
+    
+    public Item addAddress(AddressRequest request) {
+        Item Address = new Item();
+        Address.withPrimaryKey("id", request.item.id);
+        if (request.item.number!=null) Address.withString("number", request.item.number);
+        if (request.item.city!=null) Address.withString("city", request.item.city);
+        if (request.item.street!=null) Address.withString("street", request.item.street);
+        if (request.item.zipCode!=null) Address.withString("zipCode", request.item.zipCode);
+        return Address;
+    }
+    
+    public AddressResponse queryResponse(List<Item> items) {
+        AddressResponse resp = new AddressResponse("Success");
+        for(Item item: items){
+            AddressResponse.Item respItem = resp.new Item();
+            respItem.id = item.getString("id");
+            respItem.city = item.getString("city");
+            respItem.number = item.getString("number");
+            respItem.street = item.getString("street");
+            respItem.zipCode = item.getString("zipCode");
+            resp.addItem(respItem);
+        }
+        return resp;
+    }
+    
+    public AddressResponse messageResponse(String message){
+        AddressResponse resp = new AddressResponse(message);
+        return resp;
     }
 
 }
