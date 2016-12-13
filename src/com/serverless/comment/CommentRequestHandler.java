2cd1b100c78e5feab18124e38f98b237dc56904b
Lyujia Zhang <zljchloe@gmail.com>
2016-12-13 11:48:15 -0500

    Refactor Comment & Content

--- src/com/serverless/comment/CommentRequestHandler.java
+++ src/com/serverless/comment/CommentRequestHandler.java
@@ -19,6 +19,8 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.serverless.Constants;
 import com.serverless.DBManager;
+import com.serverless.customer.Customer;
+import com.serverless.customer.CustomerResponse;
 
 public class CommentRequestHandler implements RequestHandler<CommentRequest, CommentResponse> {	
 
@@ -26,137 +28,142 @@ public class CommentRequestHandler implements RequestHandler<CommentRequest, Com
     
     @Override
     public CommentResponse handleRequest(CommentRequest request, Context context) {
-        
-        // Create operation
-        if (request.operation.equals("create")) {
-        
-            // Validate id field not null
-            if (request.item.id == null) 
-                throw new IllegalArgumentException("400 Bad Request -- id is required");
-            
-            // Check existence and write item to the table 
-            PutItemSpec putItemSpec = new PutItemSpec()
-                    .withItem(addComment(request))
-                    .withConditionExpression("attribute_not_exists(id)");
-            try {
-                commentTable.putItem(putItemSpec);
-                return messageResponse("Success!");
-            } catch (ConditionalCheckFailedException e) {
-                throw new IllegalArgumentException("400 Bad Request -- id already exists");
-            }
-        }
-        
-        // Query operation
-        else if (request.operation.equals("query")) {
-            List<Item> scanResult = new ArrayList<>();
-            
-            // Look up by id
-            if (request.item.id != null && !request.item.id.isEmpty()) {
-                // Check id existence 
-                Item comment = commentTable.getItem(new PrimaryKey(Constants.COMMENT_ID_KEY, request.item.id));
-                if (comment == null) {
-                    throw new IllegalArgumentException("404 Not Found -- id does not exist");
-                }
-                // Return content with the given id
-                scanResult.add(comment);
-                return queryResponse(scanResult);
-                
-            // Look up by user
-            } else if (request.item.user != null && !request.item.user.isEmpty()) {
-            	// Return a list of comments with the given user
-                ScanRequest scanRequest = new ScanRequest().withTableName(Constants.COMMENT_TABLE_NAME);
-                ScanResult allItems = DBManager.client.scan(scanRequest);
-                for (Map<String, AttributeValue> item : allItems.getItems()){
-                    if (item.get(Constants.COMMENT_USER_KEY) != null && item.get(Constants.COMMENT_USER_KEY).getS().equals(request.item.user)) {
-                        Item comment = commentTable.getItem(Constants.COMMENT_ID_KEY, item.get(Constants.COMMENT_ID_KEY).getS());
-                        scanResult.add(comment);
-                    }
-                }
-                return queryResponse(scanResult);
-            
-            // return 10 random items
-            } else {
-            	ScanRequest scanRequest = new ScanRequest()
-            			.withTableName(Constants.COMMENT_TABLE_NAME)
-            			.withLimit(Constants.SAMPLE_SIZE);
-            	ScanResult sampleItems = DBManager.client.scan(scanRequest);
-            	
-            	// TODO: clean up this code (lots of overlapping with queryResponse)
-                CommentResponse resp = new CommentResponse("Success");
-                for (Map<String, AttributeValue> mapEntry : sampleItems.getItems()) {
-                    Comment respItem = new Comment();
-                    respItem.id       = mapEntry.get(Constants.COMMENT_ID_KEY).getS();
-                    respItem.user   = mapEntry.get(Constants.COMMENT_USER_KEY) == null ? 
-                    		null : mapEntry.get(Constants.COMMENT_USER_KEY).getS();
-                    respItem.content    = mapEntry.get(Constants.COMMENT_CONTENT_KEY) == null ? 
-                    		null : mapEntry.get(Constants.COMMENT_CONTENT_KEY).getS();
-                    respItem.comment = mapEntry.get(Constants.COMMENT_COMMENT_KEY) == null ? 
-                    		null : mapEntry.get(Constants.COMMENT_COMMENT_KEY).getS();
-                    resp.addItem(respItem);
-                }
-                
-                return resp;
-            }
-        }
-        
-        // Update operation
- 		else if (request.operation.equals("update")) {
-
- 			Map<String, String> expressName = new HashMap<>();
- 			Map<String, Object> expressValue = new HashMap<>();
-
- 			StringBuilder updateQuery = new StringBuilder();
- 			updateQuery.append("SET");
- 			if (request.item.user != null && request.item.user.length() > 0){
- 				expressName.put("#u", Constants.COMMENT_USER_KEY);
- 				expressValue.put(":val1", request.item.user);
- 				updateQuery.append(" #u = :val1,");
- 			}
- 			if (request.item.content != null && request.item.content.length() > 0){
- 				expressName.put("#c", Constants.COMMENT_CONTENT_KEY);
- 				expressValue.put(":val2", request.item.content);
- 				updateQuery.append(" #c = :val2,");
- 			}
- 			if (request.item.comment != null && request.item.comment.length() > 0){
- 				expressName.put("#m", Constants.COMMENT_COMMENT_KEY);
- 				expressValue.put(":val3", request.item.comment);
- 				updateQuery.append(" #m = :val3,");
- 			}
- 			String queryString = updateQuery.substring(0, updateQuery.length()-1);
- 			try {
- 				commentTable.updateItem("id", request.item.id, queryString,
- 						expressName, expressValue);
- 				return messageResponse("success updated");
-
- 			} catch (Exception e) {
- 				return messageResponse("failure!");
- 			}
- 		}
-        
-        // Delete operation
-        else if (request.operation.equals("delete")) {
-        	// Delete and check id existed before deletion
-        	PrimaryKey pkey = new PrimaryKey("id", request.item.id);
-            Item comment = commentTable.getItem(pkey);
-            if (comment == null) {
-            	throw new IllegalArgumentException("404 Not Found -- id does not exist");
-            }
-            
-        	commentTable.deleteItem(pkey);
-            List<Item> result = new ArrayList<>();
-            result.add(comment);
-            return queryResponse(result);
-        }
-        
-        throw new IllegalArgumentException("400 Bad Request -- invalid request");
+    	
+    	switch (request.operation) {
+    	case "create":
+        	return createComment(request.item);
+    	case "query":
+        	return queryComment(request.item);
+    	case "update":
+ 			return updateComment(request.item);
+    	case "delete":
+ 			return deleteComment(request.item);
+ 		default:
+ 			throw new IllegalArgumentException("400 Bad Request -- unsupported operation " + request.operation);
+    	}
     }
     
-    public Item addComment(CommentRequest request) {
+    public CommentResponse createComment(Comment c) {
+    	// Validate id field not null
+        if (c.id == null) 
+            throw new IllegalArgumentException("400 Bad Request -- id is required");
+        
+        // Check existence and write item to the table 
+        PutItemSpec putItemSpec = new PutItemSpec()
+                .withItem(addComment(c))
+                .withConditionExpression("attribute_not_exists(id)");
+        try {
+            commentTable.putItem(putItemSpec);
+            return messageResponse("Success!");
+        } catch (ConditionalCheckFailedException e) {
+            throw new IllegalArgumentException("400 Bad Request -- id already exists");
+        }
+    }
+    
+    public CommentResponse queryComment(Comment c) {
+    	List<Item> scanResult = new ArrayList<>();
+        
+        // Look up by id
+        if (c.id != null && !c.id.isEmpty()) {
+            // Check id existence 
+            Item comment = commentTable.getItem(new PrimaryKey(Constants.COMMENT_ID_KEY, c.id));
+            if (comment == null) {
+                throw new IllegalArgumentException("404 Not Found -- id does not exist");
+            }
+            // Return content with the given id
+            scanResult.add(comment);
+            return queryResponse(scanResult);
+            
+        // Look up by user
+        } else if (c.user != null && !c.user.isEmpty()) {
+        	// Return a list of comments with the given user
+            ScanRequest scanRequest = new ScanRequest().withTableName(Constants.COMMENT_TABLE_NAME);
+            ScanResult allItems = DBManager.client.scan(scanRequest);
+            for (Map<String, AttributeValue> item : allItems.getItems()){
+                if (item.get(Constants.COMMENT_USER_KEY) != null && item.get(Constants.COMMENT_USER_KEY).getS().equals(c.user)) {
+                    Item comment = commentTable.getItem(Constants.COMMENT_ID_KEY, item.get(Constants.COMMENT_ID_KEY).getS());
+                    scanResult.add(comment);
+                }
+            }
+            return queryResponse(scanResult);
+        
+        // return 10 random items
+        } else {
+        	ScanRequest scanRequest = new ScanRequest()
+        			.withTableName(Constants.COMMENT_TABLE_NAME)
+        			.withLimit(Constants.SAMPLE_SIZE);
+        	ScanResult sampleItems = DBManager.client.scan(scanRequest);
+        	
+        	// TODO: clean up this code (lots of overlapping with queryResponse)
+            CommentResponse resp = new CommentResponse("Success");
+            for (Map<String, AttributeValue> mapEntry : sampleItems.getItems()) {
+                Comment respItem = new Comment();
+                respItem.id       = mapEntry.get(Constants.COMMENT_ID_KEY).getS();
+                respItem.user   = mapEntry.get(Constants.COMMENT_USER_KEY) == null ? 
+                		null : mapEntry.get(Constants.COMMENT_USER_KEY).getS();
+                respItem.content    = mapEntry.get(Constants.COMMENT_CONTENT_KEY) == null ? 
+                		null : mapEntry.get(Constants.COMMENT_CONTENT_KEY).getS();
+                respItem.comment = mapEntry.get(Constants.COMMENT_COMMENT_KEY) == null ? 
+                		null : mapEntry.get(Constants.COMMENT_COMMENT_KEY).getS();
+                resp.addItem(respItem);
+            }
+            
+            return resp;
+        }
+    }
+    
+    public CommentResponse updateComment(Comment c) {
+    	Map<String, String> expressName = new HashMap<>();
+			Map<String, Object> expressValue = new HashMap<>();
+
+			StringBuilder updateQuery = new StringBuilder();
+			updateQuery.append("SET");
+			if (c.user != null && c.user.length() > 0){
+				expressName.put("#u", Constants.COMMENT_USER_KEY);
+				expressValue.put(":val1", c.user);
+				updateQuery.append(" #u = :val1,");
+			}
+			if (c.content != null && c.content.length() > 0){
+				expressName.put("#c", Constants.COMMENT_CONTENT_KEY);
+				expressValue.put(":val2", c.content);
+				updateQuery.append(" #c = :val2,");
+			}
+			if (c.comment != null && c.comment.length() > 0){
+				expressName.put("#m", Constants.COMMENT_COMMENT_KEY);
+				expressValue.put(":val3", c.comment);
+				updateQuery.append(" #m = :val3,");
+			}
+			String queryString = updateQuery.substring(0, updateQuery.length()-1);
+			try {
+				commentTable.updateItem("id", c.id, queryString,
+						expressName, expressValue);
+				return messageResponse("success updated");
+
+			} catch (Exception e) {
+				return messageResponse("failure!");
+			}
+    }
+    
+    public CommentResponse deleteComment(Comment c) {
+    	// Delete and check id existed before deletion
+    	PrimaryKey pkey = new PrimaryKey("id", c.id);
+        Item comment = commentTable.getItem(pkey);
+        if (comment == null) {
+        	throw new IllegalArgumentException("404 Not Found -- id does not exist");
+        }
+        
+    	commentTable.deleteItem(pkey);
+        List<Item> result = new ArrayList<>();
+        result.add(comment);
+        return queryResponse(result);
+    }
+    
+    public Item addComment(Comment c) {
         Item comment = new Item();
-        comment.withPrimaryKey(Constants.COMMENT_ID_KEY, request.item.id);
-        if (request.item.user!=null) comment.withString(Constants.COMMENT_USER_KEY, request.item.user);
-        if (request.item.content!=null) comment.withString(Constants.COMMENT_CONTENT_KEY, request.item.content);
-        if (request.item.comment!=null) comment.withString(Constants.COMMENT_COMMENT_KEY, request.item.comment);
+        comment.withPrimaryKey(Constants.COMMENT_ID_KEY, c.id);
+        if (c.user!=null) comment.withString(Constants.COMMENT_USER_KEY, c.user);
+        if (c.content!=null) comment.withString(Constants.COMMENT_CONTENT_KEY, c.content);
+        if (c.comment!=null) comment.withString(Constants.COMMENT_COMMENT_KEY, c.comment);
         return comment;
     }
     
--- src/com/serverless/content/ContentRequestHandler.java
+++ src/com/serverless/content/ContentRequestHandler.java
@@ -20,6 +20,8 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.serverless.Constants;
 import com.serverless.DBManager;
+import com.serverless.comment.Comment;
+import com.serverless.comment.CommentResponse;
 
 public class ContentRequestHandler implements RequestHandler<ContentRequest, ContentResponse> {	
 
@@ -27,149 +29,155 @@ public class ContentRequestHandler implements RequestHandler<ContentRequest, Con
     
     @Override
     public ContentResponse handleRequest(ContentRequest request, Context context) {
-        // Create operation
-        if (request.operation.equals("create")) {
-        
-            // Validate id field not null
-            if (request.item.id == null) 
-                throw new IllegalArgumentException("400 Bad Request -- id is required");
-            
-            // Check existence and write item to the table 
-            PutItemSpec putItemSpec = new PutItemSpec()
-                    .withItem(addContent(request))
-                    .withConditionExpression("attribute_not_exists(id)");
-            try {
-                contentTable.putItem(putItemSpec);
-                return messageResponse("Success!");
-            } catch (ConditionalCheckFailedException e) {
-                throw new IllegalArgumentException("400 Bad Request -- id already exists");
-            }
-        }
-        
-        // Query operation
-        else if (request.operation.equals("query")) {
-            List<Item> scanResult = new ArrayList<>();
-            
-            // Look up by id
-            if (request.item.id != null && !request.item.id.isEmpty()) {
-                // Check id existence 
-                Item content = contentTable.getItem(new PrimaryKey(Constants.CONTENT_ID_KEY, request.item.id));
-                if (content == null) {
-                    throw new IllegalArgumentException("404 Not Found -- id does not exist");
-                }
-                // Return content with the given id
-                scanResult.add(content);
-                return queryResponse(scanResult);
-                
-            // Look up by name
-            } else if (request.item.name != null && !request.item.name.isEmpty()) {
-            	// Return a list of contents with the given name
-                ScanRequest scanRequest = new ScanRequest().withTableName(Constants.CONTENT_TABLE_NAME);
-                ScanResult allItems = DBManager.client.scan(scanRequest);
-                for (Map<String, AttributeValue> item : allItems.getItems()){
-                    if (item.get(Constants.CONTENT_NAME_KEY) != null && item.get(Constants.CONTENT_NAME_KEY).getS().equals(request.item.name)) {
-                        Item comment = contentTable.getItem(Constants.CONTENT_ID_KEY, item.get(Constants.CONTENT_ID_KEY).getS());
-                        scanResult.add(comment);
-                    }
-                }
-                return queryResponse(scanResult);
-            
-            // return all items
-            } else {
-            	ScanRequest scanRequest = new ScanRequest()
-            			.withTableName(Constants.CONTENT_TABLE_NAME);
-            	ScanResult sampleItems = DBManager.client.scan(scanRequest);
-            	
-            	
-            	// TODO: clean up this code (lots of overlapping with queryResponse)
-                ContentResponse resp = new ContentResponse("Success");
-                for (Map<String, AttributeValue> mapEntry : sampleItems.getItems()) {
-                    Content respItem = new Content();
-                    respItem.id       = mapEntry.get(Constants.CONTENT_ID_KEY).getS();
-                    respItem.name   = mapEntry.get(Constants.CONTENT_NAME_KEY) == null ? 
-                    		null : mapEntry.get(Constants.CONTENT_NAME_KEY).getS();
-                    respItem.type    = mapEntry.get(Constants.CONTENT_TYPE_KEY) == null ? 
-                    		null : mapEntry.get(Constants.CONTENT_TYPE_KEY).getS();
-                    respItem.franchises = ContentRequestHandler.getStringList(mapEntry, Constants.CONTENT_FRANCHISES_KEY);
-                    respItem.series = ContentRequestHandler.getStringList(mapEntry, Constants.CONTENT_SERIES_KEY);
-                    respItem.episodes = ContentRequestHandler.getStringList(mapEntry, Constants.CONTENT_EPISODES_KEY);
-                    resp.addItem(respItem);
-                }
-                
-                return resp;
-            }
-        }
-        
-        // Update operation
- 		else if (request.operation.equals("update")) {
-
- 			Map<String, String> expressName = new HashMap<>();
- 			Map<String, Object> expressValue = new HashMap<>();
-
- 			StringBuilder updateQuery = new StringBuilder();
- 			updateQuery.append("SET");
- 			if (request.item.name != null && request.item.name.length() > 0){
- 				expressName.put("#n", Constants.CONTENT_NAME_KEY);
- 				expressValue.put(":val1", request.item.name);
- 				updateQuery.append(" #n = :val1,");
- 			}
- 			if (request.item.type != null && request.item.type.length() > 0){
- 				expressName.put("#t", Constants.CONTENT_TYPE_KEY);
- 				expressValue.put(":val2", request.item.type);
- 				updateQuery.append(" #t = :val2,");
- 			}
- 			if (request.item.franchises != null && request.item.franchises.size() > 0){
- 				expressName.put("#f", Constants.CONTENT_FRANCHISES_KEY);
- 				expressValue.put(":val3", request.item.franchises);
- 				updateQuery.append(" #f = :val3,");
- 			}
- 			if (request.item.series != null && request.item.series.size() > 0){
- 				expressName.put("#s", Constants.CONTENT_SERIES_KEY);
- 				expressValue.put(":val4", request.item.series);
- 				updateQuery.append(" #s = :val4,");
- 			}
- 			if (request.item.episodes != null && request.item.episodes.size() > 0){
- 				expressName.put("#e", Constants.CONTENT_EPISODES_KEY);
- 				expressValue.put(":val5", request.item.episodes);
- 				updateQuery.append(" #e = :val5,");
- 			}
- 			String queryString = updateQuery.substring(0, updateQuery.length()-1);
- 			try {
- 				contentTable.updateItem("id", request.item.id, queryString,
- 						expressName, expressValue);
- 				return messageResponse("success updated");
-
- 			} catch (Exception e) {
- 				return messageResponse("failure!");
- 			}
- 		}
-        
-        // Delete operation
-        else if (request.operation.equals("delete")) {
-        	// Delete and check id existed before deletion
-        	PrimaryKey pkey = new PrimaryKey("id", request.item.id);
-            Item content = contentTable.getItem(pkey);
-            if (content == null) {
-            	throw new IllegalArgumentException("404 Not Found -- id does not exist");
-            }
-            
-        	contentTable.deleteItem(pkey);
-            List<Item> result = new ArrayList<>();
-            result.add(content);
-            return queryResponse(result);
-        }
-        
-        throw new IllegalArgumentException("400 Bad Request -- invalid request");
+    	
+    	switch (request.operation) {
+    	case "create":
+        	return createContent(request.item);
+    	case "query":
+        	return queryContent(request.item);
+    	case "update":
+ 			return updateContent(request.item);
+    	case "delete":
+ 			return deleteContent(request.item);
+ 		default:
+ 			throw new IllegalArgumentException("400 Bad Request -- unsupported operation " + request.operation);
+    	}
     }
     
-    public Item addContent(ContentRequest request) {
+    public ContentResponse createContent(Content c) {
+    	// Validate id field not null
+        if (c.id == null) 
+            throw new IllegalArgumentException("400 Bad Request -- id is required");
+        
+        // Check existence and write item to the table 
+        PutItemSpec putItemSpec = new PutItemSpec()
+                .withItem(addContent(c))
+                .withConditionExpression("attribute_not_exists(id)");
+        try {
+            contentTable.putItem(putItemSpec);
+            return messageResponse("Success!");
+        } catch (ConditionalCheckFailedException e) {
+            throw new IllegalArgumentException("400 Bad Request -- id already exists");
+        }
+    }
+    
+    public ContentResponse queryContent(Content c) {
+    	List<Item> scanResult = new ArrayList<>();
+        
+        // Look up by id
+        if (c.id != null && !c.id.isEmpty()) {
+            // Check id existence 
+            Item content = contentTable.getItem(new PrimaryKey(Constants.CONTENT_ID_KEY, c.id));
+            if (content == null) {
+                throw new IllegalArgumentException("404 Not Found -- id does not exist");
+            }
+            // Return content with the given id
+            scanResult.add(content);
+            return queryResponse(scanResult);
+            
+        // Look up by name
+        } else if (c.name != null && !c.name.isEmpty()) {
+        	// Return a list of contents with the given name
+            ScanRequest scanRequest = new ScanRequest().withTableName(Constants.CONTENT_TABLE_NAME);
+            ScanResult allItems = DBManager.client.scan(scanRequest);
+            for (Map<String, AttributeValue> item : allItems.getItems()){
+                if (item.get(Constants.CONTENT_NAME_KEY) != null && item.get(Constants.CONTENT_NAME_KEY).getS().equals(c.name)) {
+                    Item comment = contentTable.getItem(Constants.CONTENT_ID_KEY, item.get(Constants.CONTENT_ID_KEY).getS());
+                    scanResult.add(comment);
+                }
+            }
+            return queryResponse(scanResult);
+        
+        // return all items
+        } else {
+        	ScanRequest scanRequest = new ScanRequest()
+        			.withTableName(Constants.CONTENT_TABLE_NAME);
+        	ScanResult sampleItems = DBManager.client.scan(scanRequest);
+        	
+        	
+        	// TODO: clean up this code (lots of overlapping with queryResponse)
+            ContentResponse resp = new ContentResponse("Success");
+            for (Map<String, AttributeValue> mapEntry : sampleItems.getItems()) {
+                Content respItem = new Content();
+                respItem.id       = mapEntry.get(Constants.CONTENT_ID_KEY).getS();
+                respItem.name   = mapEntry.get(Constants.CONTENT_NAME_KEY) == null ? 
+                		null : mapEntry.get(Constants.CONTENT_NAME_KEY).getS();
+                respItem.type    = mapEntry.get(Constants.CONTENT_TYPE_KEY) == null ? 
+                		null : mapEntry.get(Constants.CONTENT_TYPE_KEY).getS();
+                respItem.franchises = ContentRequestHandler.getStringList(mapEntry, Constants.CONTENT_FRANCHISES_KEY);
+                respItem.series = ContentRequestHandler.getStringList(mapEntry, Constants.CONTENT_SERIES_KEY);
+                respItem.episodes = ContentRequestHandler.getStringList(mapEntry, Constants.CONTENT_EPISODES_KEY);
+                resp.addItem(respItem);
+            }
+            
+            return resp;
+        }
+    }
+    
+    public ContentResponse updateContent(Content c) {
+    	Map<String, String> expressName = new HashMap<>();
+			Map<String, Object> expressValue = new HashMap<>();
+
+			StringBuilder updateQuery = new StringBuilder();
+			updateQuery.append("SET");
+			if (c.name != null && c.name.length() > 0){
+				expressName.put("#n", Constants.CONTENT_NAME_KEY);
+				expressValue.put(":val1", c.name);
+				updateQuery.append(" #n = :val1,");
+			}
+			if (c.type != null && c.type.length() > 0){
+				expressName.put("#t", Constants.CONTENT_TYPE_KEY);
+				expressValue.put(":val2", c.type);
+				updateQuery.append(" #t = :val2,");
+			}
+			if (c.franchises != null && c.franchises.size() > 0){
+				expressName.put("#f", Constants.CONTENT_FRANCHISES_KEY);
+				expressValue.put(":val3", c.franchises);
+				updateQuery.append(" #f = :val3,");
+			}
+			if (c.series != null && c.series.size() > 0){
+				expressName.put("#s", Constants.CONTENT_SERIES_KEY);
+				expressValue.put(":val4", c.series);
+				updateQuery.append(" #s = :val4,");
+			}
+			if (c.episodes != null && c.episodes.size() > 0){
+				expressName.put("#e", Constants.CONTENT_EPISODES_KEY);
+				expressValue.put(":val5", c.episodes);
+				updateQuery.append(" #e = :val5,");
+			}
+			String queryString = updateQuery.substring(0, updateQuery.length()-1);
+			try {
+				contentTable.updateItem("id", c.id, queryString,
+						expressName, expressValue);
+				return messageResponse("success updated");
+
+			} catch (Exception e) {
+				return messageResponse("failure!");
+			}
+    }
+    
+    public ContentResponse deleteContent(Content c) {
+    	// Delete and check id existed before deletion
+    	PrimaryKey pkey = new PrimaryKey("id", c.id);
+        Item content = contentTable.getItem(pkey);
+        if (content == null) {
+        	throw new IllegalArgumentException("404 Not Found -- id does not exist");
+        }
+        
+    	contentTable.deleteItem(pkey);
+        List<Item> result = new ArrayList<>();
+        result.add(content);
+        return queryResponse(result);
+    }
+    
+    public Item addContent(Content c) {
         Item content = new Item();
-        content.withPrimaryKey(Constants.CONTENT_ID_KEY, request.item.id);
-        if (request.item.name!=null) content.withString(Constants.CONTENT_NAME_KEY, request.item.name);
-        if (request.item.type!=null) content.withString(Constants.CONTENT_TYPE_KEY, request.item.type);
-        if (request.item.franchises!=null) content.withList(Constants.CONTENT_FRANCHISES_KEY, request.item.franchises);
-        if (request.item.series!=null) content.withList(Constants.CONTENT_SERIES_KEY, request.item.series);
-        if (request.item.episodes!=null) content.withList(Constants.CONTENT_EPISODES_KEY, request.item.episodes);
+        content.withPrimaryKey(Constants.CONTENT_ID_KEY, c.id);
+        if (c.name!=null) content.withString(Constants.CONTENT_NAME_KEY, c.name);
+        if (c.type!=null) content.withString(Constants.CONTENT_TYPE_KEY, c.type);
+        if (c.franchises!=null) content.withList(Constants.CONTENT_FRANCHISES_KEY, c.franchises);
+        if (c.series!=null) content.withList(Constants.CONTENT_SERIES_KEY, c.series);
+        if (c.episodes!=null) content.withList(Constants.CONTENT_EPISODES_KEY, c.episodes);
         return content;
     }
     
